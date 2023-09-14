c0f9ae29fe11b28b529ad5452675c998f198eb31
BrunaCamily <brunacamilydomingues@gmail.com>
2023-09-14 19:20:31 -0300

    criando classe wumpus

---
+++ src/Wumpus.java
@@ -0,0 +1,13 @@
+import java.awt.Point;
+
+public class Wumpus {
+  private Point position;
+
+  public Point getPosition() {
+    return position;
+  }
+
+  public void setPosition(Point newPosition) {
+    position = newPosition;
+  }
+}
