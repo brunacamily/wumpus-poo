4e6123b777fda9814d886f99f35d5b2ed22391d3
BrunaCamily <brunacamilydomingues@gmail.com>
2023-09-22 19:42:40 -0300

    criando turno lumpus

--- src/GameManager.java
+++ src/GameManager.java
@@ -1,4 +1,5 @@
 
+import java.util.Random;
 import java.util.Scanner;
 import java.awt.Point;
 
@@ -17,6 +18,7 @@ public class GameManager {
 
   private Jogador jogador;
   private Wumpus wumpus;
+  private Lumpus lumpus;
   private Grid grid;
   private Scanner scanner;
 
@@ -29,6 +31,7 @@ public class GameManager {
     jogador = new Jogador();
     jogador.setPosition(new Point(INITIAL_POSITION_X, INITIAL_POSITION_Y));
     wumpus = new Wumpus(new Point(14, 14));
+    lumpus = new Lumpus(new Point(14, 14));
     grid = new Grid(GRID_SIZE, new Point(INITIAL_POSITION_X, INITIAL_POSITION_Y));
     turnCount = 1;
     beginTurns();
@@ -59,6 +62,7 @@ public class GameManager {
       System.out.println("Turno " + turnCount);
       runPlayerTurn();
       runEnemyTurn();
+      runEnemy2Turn();
       turnCount++;
     }
 
@@ -116,6 +120,106 @@ public class GameManager {
     }
 
     setWumpusPosition(new Point(wumpusPosition.x + normalizedX, wumpusPosition.y));
+
+    if ( lumpus.getPosition().equals(jogador.getPosition()) ) {
+        gameOver = true;
+        endGame();
+    }
+  }
+
+  private void runEnemy2Turn() {
+    System.out.printf(
+        "Posição inicial Lumpus: (%d, %d)\n",
+        lumpus.getPosition().x,
+        lumpus.getPosition().y);
+
+    Point playerPosition = jogador.getPosition();
+    Point lumpusPosition = lumpus.getPosition();
+    double directionX = (double) playerPosition.x - lumpusPosition.x;
+    double directionY = (double) playerPosition.y - lumpusPosition.y;
+
+    int normalizedX = directionX >= 0
+        ? (int) Math.ceil(directionX / 14)
+        : (int) Math.floor(directionX / 14);
+
+    int normalizedY = directionY >= 0
+        ? (int) Math.ceil(directionY / 14)
+        : (int) Math.floor(directionY / 14);
+
+    if (Math.abs(directionX) < Math.abs(directionY)) {
+      setLumpusPosition(new Point(lumpusPosition.x, lumpusPosition.y + normalizedY));
+
+      playerPosition = jogador.getPosition();
+      lumpusPosition = lumpus.getPosition();
+      directionX =  playerPosition.x - lumpusPosition.x;
+      directionY =  playerPosition.y - lumpusPosition.y;
+
+      normalizedX = directionX >= 0
+          ? (int) Math.ceil(directionX / 14)
+          : (int) Math.floor(directionX / 14);
+
+      if ( lumpus.getPosition().equals(jogador.getPosition()) ) {
+        gameOver = true;
+        endGame();
+      } else if ( normalizedX == 0 ) {
+        Random aleatorio = new Random();
+        int aux = aleatorio.nextInt((10 - 1) + 1) + 1;
+
+        normalizedX = aux%2==0
+          ? 1
+          : -1;
+
+        boolean aux2 = grid.isValidPosition(new Point(lumpusPosition.x + normalizedX, lumpusPosition.y));
+
+        if (aux2 == false && normalizedX == 1)
+          normalizedX = -1;
+        else if (aux2 == false && normalizedX == -1)
+          normalizedX = 1;
+      }
+      setLumpusPosition(new Point(lumpusPosition.x + normalizedX, lumpusPosition.y));
+
+      if ( lumpus.getPosition().equals(jogador.getPosition()) ) {
+        gameOver = true;
+        endGame();
+      }
+      return;
+    }
+
+    setLumpusPosition(new Point(lumpusPosition.x + normalizedX, lumpusPosition.y));
+
+    playerPosition = jogador.getPosition();
+    lumpusPosition = lumpus.getPosition();
+    directionX = playerPosition.x - lumpusPosition.x;
+    directionY = playerPosition.y - lumpusPosition.y;
+
+    normalizedY = directionY >= 0
+        ? (int) Math.ceil(directionY / 14)
+        : (int) Math.floor(directionY / 14);
+
+    if ( lumpus.getPosition().equals(jogador.getPosition() )) {
+        gameOver = true;
+        endGame();
+      } else if ( normalizedY == 0 ) {
+        Random aleatorio = new Random();
+        int aux = aleatorio.nextInt((10 - 1) + 1) + 1;
+
+        normalizedY = aux%2==0
+          ? 1
+          : -1;
+
+        boolean aux2 = grid.isValidPosition(new Point(lumpusPosition.x, lumpusPosition.y + normalizedY));
+        if (aux2 == false && normalizedY == 1)
+          normalizedY = -1;
+        else if (aux2 == false && normalizedY == -1)
+          normalizedY = 1;
+      }
+
+    setLumpusPosition(new Point(lumpusPosition.x, lumpusPosition.y + normalizedY));
+
+    if ( lumpus.getPosition().equals(jogador.getPosition()) ) {
+        gameOver = true;
+        endGame();
+    }
   }
 
   private boolean makeAction(String input) {
@@ -157,5 +261,13 @@ public class GameManager {
         "Posição final wumpus: (%d, %d)\n",
         wumpus.getPosition().x,
         wumpus.getPosition().y);
+  }
+
+  private void setLumpusPosition(Point newPosition) {
+    lumpus.setPosition(newPosition);
+    System.out.printf(
+        "Posição final Lumpus: (%d, %d)\n",
+        lumpus.getPosition().x,
+        lumpus.getPosition().y);
   }
 }
